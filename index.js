b707dd1d02a0820f9e92d55cc72a055de160d357
Vinahrad Pavel <vino49@yandex.ru>
2019-12-16 15:04:37 +0300

    get request age

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
+/* eslint-disable padded-blocks */
+/* eslint-disable arrow-parens */
+/* eslint-disable consistent-return */
 const express = require('express');
 
 const app = express();
-// определяем обработчик для маршрута "/"
-app.get('/', (request, response) => {
-  // отправляем ответ
-  response.send('<h2>Привет Expresss!</h2>');
+
+const users = [
+  { name: 'Ivan', age: 18 },
+  { name: 'Vasya', age: 22 },
+  { name: 'Pavel', age: 25 },
+  { name: 'Denis', age: 60 },
+  { name: 'Ilya', age: 41 },
+  { name: 'Olga', age: 22 },
+];
+
+app.get('/users', (request, response) => {
+  const { query } = request;
+  const { age } = query;
+
+  if (age) {
+    return response.json({
+      users: users.filter(it => it.age > age),
+    });
+  }
+
 });
+
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);

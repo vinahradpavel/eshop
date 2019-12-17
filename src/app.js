77ba3f6dc2ecf2974d586fe00b4e647dcdbf1190
Vinahrad Pavel <vino49@yandex.ru>
2019-12-17 17:17:55 +0300

    get post delete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,68 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const Users = require('./models/users');
 
-mongoose.connect('mongodb://localhost:27017/usersdb', { useNewUrlParser: true, useUnifiedTopology: true });
-(async () => {
-  const users = await Users.find().select("name");
-  console.log(users);
-})()
+mongoose.connect('mongodb://localhost:27017/usersdb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 
+// (async () => {
+//   const users = await Users.create({
+//     name: 'Ivan',
+//     surname: 'Ivanov',
+//     secondName: 'Ivanovich',
+//     age: 25,
+//   });
+//   console.log(users);
+// })();
 
+app.get('/users', async (req, res) => {
+  const users = await Users.find();
+
+  return res.json({
+    users,
+  });
+});
+
+app.get('/users/:name', async (req, res) => {
+  const { params } = req;
+  const { name } = params;
+
+  const user = await Users.find({ name });
+  return res.json({
+    user,
+  });
+});
+
+app.post('/users', async (req, res) => {
+  const user = await Users.create(
+    req.body,
+  );
+
+  return res.json({
+    user,
+  });
+});
+
+app.delete('/users/:id', async (req, res) => {
+  const { params } = req;
+  const { id } = params;
+
+  const users = await Users.findByIdAndRemove(id);
+
+  return res.json({
+    users,
+  });
+});
+
+app.listen(3000);

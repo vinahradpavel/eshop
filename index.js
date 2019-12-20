4314eca69ae3a2dcec73301c40ef4b367506fd81
Vinahrad Pavel <vino49@yandex.ru>
2019-12-20 17:11:45 +0300

    .env

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-require('dotenv').config();
+// process.env.NODE_ENV === 'production' ?
+const path = process.env.NODE_ENV === 'test' ? '.env.test' : '.env.development';
+require('dotenv').config({ path });
+
+console.log(process.env);
 require('./src/app');
--- src/routes/auth/public.js
+++ src/routes/auth/public.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-require('dotenv').config();
+// require('dotenv').config();
 const Users = require('../../models/users');
 
 const router = express.Router();

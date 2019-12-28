80e5897dd3d0db4c40e96ed3e130db67be4c8963
Vinahrad Pavel <vino49@yandex.ru>
2019-12-28 16:58:55 +0300

    update errors

--- src/routes/users/private.js
+++ src/routes/users/private.js
@@ -8,14 +8,14 @@ const { ADMIN } = ROLES;
 
 const router = express.Router();
 
-router.get('/', roleAccess({ roles: [ADMIN] }), async (req, res) => {
+router.get('/', roleAccess({ roles: [ADMIN] }), async (req, res, next) => {
   try {
     const users = await Users.find().lean();
     return res.status(200).json({
       users,
     });
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 });
 

8241d0051c76d0c11c60e5826cdd983d41799c97
Vinahrad Pavel <vino49@yandex.ru>
2020-01-04 11:05:46 +0300

    update last commit

---
+++ src/routes/brands/private.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const { celebrate } = require('celebrate');
+const Brands = require('../../models/brands');
+const roleAccess = require('../../middlewares/roleAccess');
+
+const { ROLES } = require('../../constants/users');
+
+const { ADMIN } = ROLES;
+
+const { brandsPost, brandsDelete } = require('../../validators/brands');
+
+const router = express.Router();
+
+router.post('/', roleAccess({ roles: [ADMIN] }), celebrate(brandsPost), async (req, res, next) => {
+  try {
+    const brand = await Brands.create(
+      req.body,
+    );
+    // const categoryData = await Categories.find({}).populate('subCategories').lean();
+    return res.status(200).json({
+      brand,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/', roleAccess({ roles: [ADMIN] }), celebrate(brandsDelete), async (req, res, next) => {
+  try {
+    const { name } = req.query;
+    const brand = await Brands.deleteOne({ name });
+    return res.status(200).json({
+      brand,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+module.exports = router;
